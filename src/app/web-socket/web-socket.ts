ec2c04f77d2e6c8a1afd9a6cdf55cc774d11a5e6
YuliAdam <ylia_adamovich@mail.ru>
2025-04-22 16:13:18 +0200

    fix: change url of connection

--- src/app/web-socket/web-socket.ts
+++ src/app/web-socket/web-socket.ts
@@ -31,7 +31,8 @@ export class MyWebSocket {
     if (document.location.protocol === "https:") {
       scheme += "s";
     }
-    const serverUrl = scheme + "://" + document.location.hostname + ":4000";
+    const serverUrl =
+      scheme + "://" + "fun-chat-yuliadam.vercel.app" + ":4000";
     const connection = new WebSocket(serverUrl, "json");
     connection.onopen = function () {
       console.log("CONNECTED");
